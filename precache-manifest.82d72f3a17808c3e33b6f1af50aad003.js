2729b7b9a81ab496c35add5910c77f2f6a3960a5
alittlebyte <vladislavnikora@gmail.com>
2020-07-18 13:01:48 +0300

    Updates

---
+++ precache-manifest.82d72f3a17808c3e33b6f1af50aad003.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fbaae20552b68382a6c6211e9ccca6a",
+    "url": "/kusdix/index.html"
+  },
+  {
+    "revision": "5c5d9ef6bbd43a67b071",
+    "url": "/kusdix/static/css/main.b7599929.chunk.css"
+  },
+  {
+    "revision": "7dd994ec1c9f3d7db457",
+    "url": "/kusdix/static/js/2.5c7949d1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kusdix/static/js/2.5c7949d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c5d9ef6bbd43a67b071",
+    "url": "/kusdix/static/js/main.1dd643ca.chunk.js"
+  },
+  {
+    "revision": "399f757417e06871f36a",
+    "url": "/kusdix/static/js/runtime-main.c7abd216.js"
+  },
+  {
+    "revision": "59b3d60dec6eb931757dd5911a19d4ea",
+    "url": "/kusdix/static/media/duck.59b3d60d.png"
+  }
+]);
